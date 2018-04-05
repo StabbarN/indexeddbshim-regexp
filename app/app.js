3821b2959bb1ad920021fc1ee370c6c443efe29c
StabbarN <[omitted]>
2018-04-05 11:34:52 +0200

    First init. Working fine with 'useBuiltIns: false' and '@babel/polyfill' added to entry

---
+++ app/app.js
@@ -0,0 +1,35 @@
+// import 'babel-polyfill'
+// import '@babel/polyfill'
+
+import 'indexeddbshim'
+window.shimIndexedDB.__useShim()
+import Dexie from 'dexie'
+
+(function() {
+  const shimIndexedDB = window.shimIndexedDB
+  const shimIDBKeyRange = window.IDBKeyRange
+
+  const db = new Dexie('testDBshim', {
+    autoOpen: false,
+    indexedDB: shimIndexedDB,
+    IDBKeyRange: shimIDBKeyRange,
+  })
+  db.version(1).stores({
+    logs: '++id',
+  })
+  console.log('Will open DB')
+  // alert('Will open DB')
+  db.open().then(() => {
+    console.log('Did open DB')
+    // alert('Did open DB')
+
+    const testLog = {
+      message: 'first log'
+    }
+    return db['logs'].add(testLog)
+  }).then(() => {
+    alert('Did add first log')
+  }).catch((err) => {
+    alert('Caught an error:' + err)
+  })
+})();
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require( 'path' ) // eslint-disable-line no-undef
+
+const config = {
+  entry: [
+    // 'babel-polyfill',
+    '@babel/polyfill',
+    './app/app.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)?$/,
+        loader: 'babel-loader',
+        options: {
+          babelrc: false,
+          presets: [
+            ['@babel/preset-env', {
+              useBuiltIns: false,
+              targets: {
+                browsers: [
+                  'iOS >= 9',
+                ],
+              },
+            }],
+          ],
+          plugins: [
+            // '@babel/plugin-transform-sticky-regex',
+          ],
+        },
+      }
+    ],
+  },
+}
+module.exports = config
